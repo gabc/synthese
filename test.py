ca0a2f2a9758bf5792282a9e34b0dd4aab42b700
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2015-02-07 12:22:28 -0500

    Changez de côté, vous vous êtes trompé!

--- test.py
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env python3
-
-from note import scm2js
-
-assert (scm2js("(define foo (lambda (x) (+ x x)))") == 'var foo = function(x) {return(x + x)}')
-assert (scm2js("(define foo (lambda (x) (if (= x 2) (+ x x))))") == 'var foo = function(x) {if(x === 2) {return(x + x)}}')
-
-
